51e34bf8fca71100dbc55115fb10d279c122400c
David Jay <davidgljay@gmail.com>
2025-03-15 19:52:50 -0700

    Adding community model with get function

---
+++ src/models/community.js
@@ -0,0 +1,32 @@
+const { graphql } = require('../apis/graphql');
+
+class Community {
+    constructor(id, name, data = {}) {
+        this.id = id;
+        this.name = name;
+        this.data = data;
+    }
+
+    async get(bot_phone) {
+        const query = `
+query GetCommunities($bot_phone:String!) {
+  communities(where: {bot_phone: {_eq: $bot_phone}}) {
+	id
+    name
+    onboarding_id
+  }
+}
+`;
+
+        const variables = { bot_phone };
+
+        const response = await graphql(query, variables);
+        if (response.errors) {
+            throw new Error(response.errors[0].message);
+        }
+        const community = response.data.communities[0];
+        return new Community(community.id, community.name, community);
+    }
+}
+
+module.exports = Community;
---
+++ src/tests/community.test.js
@@ -0,0 +1,48 @@
+const Community = require('../models/community');
+const { graphql } = require('../apis/graphql');
+
+jest.mock('../apis/graphql', () => ({
+    graphql: jest.fn()
+}));
+
+describe('Community Model', () => {
+    afterEach(() => {
+        jest.clearAllMocks();
+    });
+
+    it('should fetch community data successfully', async () => {
+        const bot_phone = '+1234567890';
+        const mockResponse = {
+            data: {
+                communities: [
+                    { id: 1, name: 'Test Community' }
+                ]
+            }
+        };
+
+        graphql.mockResolvedValue(mockResponse);
+
+        const community = await new Community().get(bot_phone);
+
+        expect(graphql).toHaveBeenCalledWith(expect.any(String), { bot_phone });
+        expect(community).toEqual(new Community(1, 'Test Community', { id: 1, name: 'Test Community' }));
+    });
+
+    it('should throw an error if graphql returns errors', async () => {
+        const bot_phone = '+1234567890';
+        const mockError = new Error('GraphQL error');
+        graphql.mockResolvedValue({ errors: [mockError] });
+
+        await expect(new Community().get(bot_phone)).rejects.toThrow('GraphQL error');
+        expect(graphql).toHaveBeenCalledWith(expect.any(String), { bot_phone });
+    });
+
+    it('should throw an error if graphql request fails', async () => {
+        const bot_phone = '+1234567890';
+        const mockError = new Error('Network error');
+        graphql.mockRejectedValue(mockError);
+
+        await expect(new Community().get(bot_phone)).rejects.toThrow('Network error');
+        expect(graphql).toHaveBeenCalledWith(expect.any(String), { bot_phone });
+    });
+});
