e201a18e82fd9b471d4a0be805e750d615e9ce98
David Jay <davidgljay@gmail.com>
2025-04-14 23:20:58 -0700

    Only triggering announcement script for admins.

--- src/helpers/hashtag_commands.js
+++ src/helpers/hashtag_commands.js
@@ -4,6 +4,9 @@ import Script from "../models/script";
 export async function bot_message_hashtag(hashtag, membership, community, message) {
     switch(hashtag) {
         case '#announcement':
+            if (membership.type !== 'admin') {
+                return false;
+            }
             // Get system script for announcement
             const announcement_script = await Script.get_system_script('announcement');
             if (!announcement_script) {
