69bfed636c321a11efcc63550a6a2326bf7d9243
David Jay <davidgljay@gmail.com>
2025-04-15 22:57:01 -0700

    Updating hashtag_commands tests.

--- src/tests/hashtag_commands.test.js
+++ src/tests/hashtag_commands.test.js
@@ -13,7 +13,7 @@ describe("bot_message_hashtag", () => {
     describe("#announcement", () => {
 
         it("should handle #announcement hashtag correctly", async () => {
-            const mockMembership = { id: 1 };
+            const mockMembership = { id: 1 , type: 'admin'};
             const mockCommunity = {};
             const mockMessage = "Test message";
             const mockScript = {
@@ -25,8 +25,9 @@ describe("bot_message_hashtag", () => {
             Script.get_system_script = jest.fn().mockResolvedValue(mockScript);
             Membership.set_variable = jest.fn();
 
-            await bot_message_hashtag("#announcement", mockMembership, mockCommunity, mockMessage);
+            const send_message = await bot_message_hashtag("#announcement", mockMembership, mockCommunity, mockMessage);
 
+            expect(send_message).toBe(true);
             expect(Script.get_system_script).toHaveBeenCalledWith("announcement");
             expect(Membership.set_variable).toHaveBeenCalledWith(mockMembership.id, "current_script_id", mockScript.id);
             expect(Membership.set_variable).toHaveBeenCalledWith(mockMembership.id, "step", "0");
@@ -34,8 +35,20 @@ describe("bot_message_hashtag", () => {
             expect(mockScript.send).toHaveBeenCalledWith("0");
         });
 
+        it("should return false if membership type is not admin", async () => {
+            const mockMembership = { id: 1, type: 'user' };
+            const mockCommunity = {};
+            const mockMessage = "Test message";
+
+            const result = await bot_message_hashtag("#announcement", mockMembership, mockCommunity, mockMessage);
+
+            expect(result).toBe(false);
+            expect(Script.get_system_script).not.toHaveBeenCalled();
+            expect(Membership.set_variable).not.toHaveBeenCalled();
+        });
+
         it("should do nothing if system script for #announcement is not found", async () => {
-            const mockMembership = { id: 1 };
+            const mockMembership = { id: 1 , type: 'admin' };
             const mockCommunity = {};
             const mockMessage = "Test message";
 
