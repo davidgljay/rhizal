060d09fbb7e8765b45cb6ffab09cc18512c929f1
David Jay <davidgljay@gmail.com>
2025-08-31 11:43:06 -0700

    Updating app.js to run the init scripts if no community is found.

--- src/app.js
+++ src/app.js
@@ -1,9 +1,24 @@
 const webSocketManager = require('./apis/signal');
 const { receive_raw_message } = require('./routes/ws');
 const Community = require('./models/community');
+const { load_sql_schema, upload_metadata, create_system } = require("./initialization/db_init.js");
 
-Community.get_bot_phones().then(bot_phones => {
+Community.get_bot_phones()
+    .then(bot_phones => {
     bot_phones.forEach(bot_phone => {
         webSocketManager.connect(receive_raw_message, bot_phone);
     });
-});
+    })
+    .catch(async (err) => {
+        console.log(err)
+        if (err.message && err.message.includes("field 'communities' not found")) {
+            console.log('Looks like a fresh install!')
+            console.log('Setting up DB schema...')
+            await load_sql_schema();
+            console.log('Loading metadata to Hasura...')
+            await upload_metadata();
+            console.log('Setting up system-level scripts...')
+        } else {
+            throw err;
+        }
+    });
