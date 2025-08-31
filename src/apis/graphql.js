dd56f312c1395104b01a980c109789b5f4ba10d2
David Jay <davidgljay@gmail.com>
2025-08-31 11:40:45 -0700

    Hard coding graphql URL and adding more helpful error messaging.

--- src/apis/graphql.js
+++ src/apis/graphql.js
@@ -1,8 +1,8 @@
 const fetch = require('node-fetch');
 
 export async function graphql(query, variables) {
-    const GRAPHQL_URL =  process.env.GRAPHQL_URL;
-    const GRAPHQL_ADMIN_SECRET = process.env.GRAPHQL_ADMIN_SECRET;
+    const GRAPHQL_URL =  'http://graphql-engine:8080/v1/graphql';
+    const GRAPHQL_ADMIN_SECRET = process.env.HASURA_GRAPHQL_ADMIN_SECRET;
     const GRAPHQL_AUTH_HEADER = process.env.GRAPHQL_AUTH_HEADER || 'x-hasura-admin-secret';
     const response = await fetch(GRAPHQL_URL, {
         method: 'POST',
@@ -15,6 +15,8 @@ export async function graphql(query, variables) {
 
     const data = await response.json();
     if (!response.ok || data.errors) {
+        console.log(GRAPHQL_URL)
+        console.log(response.status)
         throw new Error(data.errors ? data.errors[0].message : 'Unknown error');
     }
 
