853c9af814ce2333170d3d81d32a030926495705
David Jay <davidgljay@gmail.com>
2025-10-04 13:16:32 -0700

    Making module handlind consistent in graphql api wrapper.

--- src/apis/graphql.js
+++ src/apis/graphql.js
@@ -1,6 +1,6 @@
 const fetch = require('node-fetch');
 
-export async function graphql(query, variables) {
+async function graphql(query, variables) {
     const GRAPHQL_URL =  'http://graphql-engine:8080/v1/graphql';
     const GRAPHQL_ADMIN_SECRET = process.env.HASURA_GRAPHQL_ADMIN_SECRET;
     const GRAPHQL_AUTH_HEADER = process.env.GRAPHQL_AUTH_HEADER || 'x-hasura-admin-secret';
@@ -15,11 +15,11 @@ export async function graphql(query, variables) {
 
     const data = await response.json();
     if (!response.ok || data.errors) {
-        console.log(GRAPHQL_URL)
-        console.log(response.status)
         throw new Error(data.errors ? data.errors[0].message : 'Unknown error');
     }
 
     return data;
 }
+
+module.exports = { graphql };
 
