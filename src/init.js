ff0ef13fb9e0a6587e8ca544d974cf756c8554aa
David Jay <davidgljay@gmail.com>
2025-10-04 13:15:59 -0700

    Updating init script to await properly.

--- src/init.js
+++ src/init.js
@@ -1,7 +1,11 @@
 const { load_sql_schema, upload_metadata, create_system } = require('./initialization/db_init');
-const { update_community_and_scripts } = require ('./initialization/script_sync');
+const { update_community_and_scripts } = require('./initialization/script_sync');
 
-load_sql_schema();
-upload_metadata();
-create_system();
-update_community_and_scripts();
+async function initialize() {
+    await load_sql_schema();
+    await upload_metadata();
+    await create_system();
+    await update_community_and_scripts();
+}
+
+initialize().catch(console.error);
