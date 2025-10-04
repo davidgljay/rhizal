26ec12b2beef7dafc15fae767d909616cb733768
David Jay <davidgljay@gmail.com>
2025-10-04 14:27:57 -0700

    Adding log statements for clarity.

--- src/init.js
+++ src/init.js
@@ -2,9 +2,13 @@ const { load_sql_schema, upload_metadata, create_system } = require('./initializ
 const { update_community_and_scripts } = require('./initialization/script_sync');
 
 async function initialize() {
+    console.log('Initializing database');
     await load_sql_schema();
+    console.log('Uploading metadata');
     await upload_metadata();
+    console.log('Creating system');
     await create_system();
+    console.log('Updating community and scripts');
     await update_community_and_scripts();
 }
 
