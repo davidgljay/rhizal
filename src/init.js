986f25de9b52755c2a716f690ada19b9e5019680
David Jay <davidgljay@gmail.com>
2025-10-04 17:57:24 -0700

    Adding initial message to ensure that configuration is in place.

--- src/init.js
+++ src/init.js
@@ -1,7 +1,8 @@
 const { load_sql_schema, upload_metadata, create_system } = require('./initialization/db_init');
-const { update_community_and_scripts } = require('./initialization/script_sync');
+const { update_community_and_scripts, initial_message } = require('./initialization/script_sync');
 
 async function initialize() {
+    await initial_message();
     console.log('Initializing database');
     await load_sql_schema();
     console.log('Uploading metadata');
--- src/initialization/script_sync.js
+++ src/initialization/script_sync.js
@@ -4,6 +4,26 @@ const yaml = require('js-yaml');
 const Community = require('../models/community');
 const Script = require('../models/script');
 
+async function initial_message() {
+    console.log("Thank you for trying out Rhizal!");
+    console.log("This initialization script will set up Rhizal's data stores and configure settings for your organization.");
+    console.log("In the /scripts_config directory there are a series of yaml files. Most of them are scripts, you can modify them to modify how Rhizal will respond as people engage with it. See the documentation on Github for more information.");
+    console.log("There is also a file called community_config.yml. This Includes information about your organization and the number that Rhizal will use to establish a Signal account.");
+    console.log("Please edit this file to set a number where you can receive text messages. It should NOT be a number already associated with an account on Signal, as this could wind up disrupting your other communications.");
+    console.log("Once you have saved this file with a proper number and other information, press any key to continue.");
+
+    await new Promise((resolve) => {
+        process.stdin.setRawMode(true);
+        process.stdin.resume();
+        process.stdin.once('data', () => {
+            process.stdin.setRawMode(false);
+            process.stdin.pause();
+            resolve();
+        });
+    });
+}
+
+
 const create_or_update_community = async function () {
     const community_config_yaml = fs.readFileSync(path.join(__dirname, '../../scripts_config', 'community_config.yml'), 'utf8');
     let community_config;
@@ -67,6 +87,7 @@ const update_community_and_scripts = async function () {
 }
 
 module.exports = {
+    initial_message,
     update_community_and_scripts,
     create_or_update_community,
     create_or_update_script
