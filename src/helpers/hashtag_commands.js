e516c2ebcd8f7656b76a52eab43eaabfd17ff602
David Jay <davidgljay@gmail.com>
2025-11-02 21:52:51 -0800

    Adding "#cancel" command.

--- src/helpers/hashtag_commands.js
+++ src/helpers/hashtag_commands.js
@@ -2,13 +2,12 @@ import Membership from "../models/membership";
 import Script from "../models/script";
 
 export async function bot_message_hashtag(hashtag, membership, community, message) {
+    const announcement_script = await Script.get_system_script('announcement');
     switch(hashtag) {
         case '#announcement':
             if (!membership.permissions.includes('announcement')) {
                 return false;
             }
-            // Get system script for announcement
-            const announcement_script = await Script.get_system_script('announcement');
             if (!announcement_script) {
                 return;
             }
@@ -16,6 +15,14 @@ export async function bot_message_hashtag(hashtag, membership, community, messag
             await Membership.set_variable(membership.id, 'step', '0');
             await announcement_script.get_vars(membership, message);
             await announcement_script.send('0');
+            return true;
+        case '#cancel':
+            if (!announcement_script) {
+                return false;
+            }
+            await announcement_script.get_vars(membership, message);
+            await announcement_script.send('3');
+            await Membership.set_variable(membership.id, 'step', 'done');
             return true;
     }
     return false;
